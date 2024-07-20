76cce768e01ea7e7809b05d638edccc4c8bb78b6
Serhii Schastlivtsev <shi@roboticket.com>
2024-07-20 05:17:22 +0300

    fix: add message after order sending

--- src/modules/Contact/features/Footer/index.tsx
+++ src/modules/Contact/features/Footer/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 
 import Image from 'next/image';
 import Link from 'next/link';
@@ -16,22 +16,40 @@ import styles from './style.module.css';
 export const Footer = () => {
   const t = useTranslations();
   const { orderData } = useAppSelector((state) => state.ContactReducer);
+  const [isSended, setIsSended] = useState(false);
 
   const onSendOrderDataHandler = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
-    await contactApi.addOrder(orderData);
+    await contactApi
+      .addOrder(orderData)
+      .then(() => setIsSended(true))
+      .catch(() => console.log('Failed'));
   };
+
+  useEffect(() => {
+    if (isSended) {
+      setTimeout(() => {
+        setIsSended(false);
+      }, 5000);
+    }
+  }, [isSended]);
 
   return (
     <footer className={styles.footer}>
-      <p className={styles.privacy}>
-        {t('policy-text')}
-        <Link href="/#">{t('policy-link')}</Link>
-      </p>
-      <button className={styles.send} onClick={(e) => onSendOrderDataHandler(e)}>
-        <Image src={SendIcon} alt="send icon" />
-        {t('send')}
-      </button>
+      {isSended ? (
+        <p className={styles.sended}>{t('successfully-shipped')}</p>
+      ) : (
+        <Fragment>
+          <p className={styles.privacy}>
+            {t('policy-text')}
+            <Link href="/#">{t('policy-link')}</Link>
+          </p>
+          <button className={styles.send} onClick={(e) => onSendOrderDataHandler(e)}>
+            <Image src={SendIcon} alt="send icon" />
+            {t('send')}
+          </button>
+        </Fragment>
+      )}
     </footer>
   );
 };
