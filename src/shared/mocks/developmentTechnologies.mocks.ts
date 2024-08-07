2c0b99bb818e3eb8fc8516fb6d90f22ab8b05da5
Serhii Schastlivtsev <shi@roboticket.com>
2024-08-08 01:13:07 +0300

    fix: add new icons in services section

--- src/shared/mocks/developmentTechnologies.mocks.ts
+++ src/shared/mocks/developmentTechnologies.mocks.ts
@@ -1,13 +1,22 @@
 import { TechnologiesData } from '@shared/interfaces/TechnologiesData.interfaces';
 
+import GoIcon from '@public/icons/GoIcon.svg';
 import GraphIcon from '@public/icons/GraphIcon.svg';
 import LevelFiveIcon from '@public/icons/LevelFiveIcon.svg';
 import LevelFourIcon from '@public/icons/LevelFourIcon.svg';
+import MongoIcon from '@public/icons/MongoIcon.svg';
+import NestIcon from '@public/icons/NestIcon.svg';
 import NextIcon from '@public/icons/NextIcon.svg';
+import NodeIcon from '@public/icons/NodeIcon.svg';
+import PostgreIcon from '@public/icons/PostgreIcon.svg';
 import ReactIcon from '@public/icons/ReactIcon.svg';
 import ReduxIcon from '@public/icons/ReduxIcon.svg';
+import ShopifyIcon from '@public/icons/ShopifyIcon.svg';
+import TildaIcon from '@public/icons/TildaIcon.svg';
 import TSIcon from '@public/icons/TSIcon.svg';
+import WebflowIcon from '@public/icons/WebflowIcon.svg';
 import WebpackIcon from '@public/icons/WebpackIcon.svg';
+import WordPressIcon from '@public/icons/WordPressIcon.svg';
 
 export const developmentTechnologies: TechnologiesData[] = [
   {
@@ -48,43 +57,55 @@ export const developmentTechnologies: TechnologiesData[] = [
   },
   {
     title: 'Nest',
-    icon: GraphIcon,
+    icon: NestIcon,
     levelIcon: LevelFiveIcon,
     fieldOfUsage: 'backend',
   },
   {
     title: 'Go',
-    icon: GraphIcon,
+    icon: GoIcon,
     levelIcon: LevelFiveIcon,
     fieldOfUsage: 'backend',
   },
   {
     title: 'Node.js',
-    icon: GraphIcon,
+    icon: NodeIcon,
     levelIcon: LevelFiveIcon,
     fieldOfUsage: 'backend',
   },
   {
     title: 'PostgreSQL',
-    icon: GraphIcon,
+    icon: PostgreIcon,
     levelIcon: LevelFiveIcon,
     fieldOfUsage: 'database',
   },
   {
     title: 'MongoDB',
-    icon: GraphIcon,
+    icon: MongoIcon,
     levelIcon: LevelFiveIcon,
     fieldOfUsage: 'database',
   },
   {
     title: 'Wordpress',
-    icon: GraphIcon,
+    icon: WordPressIcon,
     levelIcon: LevelFiveIcon,
     fieldOfUsage: 'cms',
   },
   {
     title: 'Shopyfi',
-    icon: GraphIcon,
+    icon: ShopifyIcon,
+    levelIcon: LevelFiveIcon,
+    fieldOfUsage: 'cms',
+  },
+  {
+    title: 'Tilda',
+    icon: TildaIcon,
+    levelIcon: LevelFiveIcon,
+    fieldOfUsage: 'cms',
+  },
+  {
+    title: 'Webflow',
+    icon: WebflowIcon,
     levelIcon: LevelFiveIcon,
     fieldOfUsage: 'cms',
   },
