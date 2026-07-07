29f3a6de2734da3201810db5a9294b754aa9b5c4
Dmytro Kazantsev <kwenkazantsevv@gmail.com>
2026-07-07 17:05:15 +0200

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,31 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             gtag('config', '${GA_MEASUREMENT_ID}');
           `}
         </Script>
+        <Script id="linkedin-insight-init" strategy="afterInteractive">
+          {`
+            _linkedin_partner_id = "9341226";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+            (function(l) {
+              if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q=[]}
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);})(window.lintrk);
+          `}
+        </Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=9341226&fmt=gif"
+          />
+        </noscript>
         {children}
       </body>
     </html>
