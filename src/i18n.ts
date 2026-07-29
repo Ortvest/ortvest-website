48b6396d06c18c0fb9afd95c1807cc0122692ceb
Dmytro Kazantsev <kwenkazantsevv@gmail.com>
2026-07-29 14:26:19 +0200

    1

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,13 +3,10 @@ import { getRequestConfig } from 'next-intl/server';
 
 const locales = ['en', 'ua', 'pl'];
 
-export default getRequestConfig(async ({ requestLocale }) => {
-  const locale = await requestLocale;
-
+export default getRequestConfig(async ({ locale }) => {
   if (!locale || !locales.includes(locale)) notFound();
 
   return {
-    locale,
     messages: (await import(`../messages/${locale}.json`)).default,
   };
 });
